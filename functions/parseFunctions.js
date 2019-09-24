160c35ebfd0788ffc69e75e3cf1e15c6934c1518
Fresh <fresh1736@gmail.com>
2019-09-24 02:24:51 -0500

    Added filters for all (most?) JSON junk tags

--- functions/parseFunctions.js
+++ functions/parseFunctions.js
@@ -1,5 +1,45 @@
 var books = require('../5eTools/data/books.json');
 
+class tags {
+    constructor(input) {
+        this.input = input;
+    }
+    removeFilters(){
+        this.input = filterFunc("filter", this.input, false)
+        return this;
+    }
+    removeBooks(){
+        this.input = filterFunc("book", this.input, false)
+        return this;
+    }
+    removeSpells(){
+        this.input = filterFunc("spell", this.input, true)
+        return this;
+    }
+    removeConditions(){
+        this.input = filterFunc("condition", this.input, true)
+        return this;
+    }
+    removeItems(){
+        this.input = filterFunc("item", this.input, false)
+        return this;
+    }
+    removeDice(){
+        this.input = filterFunc("dice", this.input, true)
+        return this;
+    }
+    removeClass(){
+        this.input = filterFunc("class", this.input, false)
+        return this;
+    }
+    toString(){
+        return this.input;
+    }
+}
+
+exports.removeTags = function(input){
+    return new tags(input).removeFilters().removeBooks().removeSpells().removeConditions().removeItems().removeDice().removeClass().toString();
+}
 exports.parseSources = function(source){
     let bookName = source;
     books.book.forEach(book=>{
@@ -9,7 +49,6 @@ exports.parseSources = function(source){
     })
     return bookName;
 }
-
 exports.parseTable = function(tableObject){
     let description = "";
     for(let i in tableObject.colLabels){
@@ -26,19 +65,23 @@ exports.parseTable = function(tableObject){
     }
     return description;
 }
-
-exports.removeFilters = function(input){
+function filterFunc(regex, input, isSimple){
     let description = input, filteredMessage = "";
-    if(description.match(/{@filter.*}/)){
-        description = description.split(/(?=\{@filter.*})/);
+    var fullRegex = new RegExp("{@" + regex + ".*}"),
+        lookaheadRegex = new RegExp("(?=\\{@" + regex + ".*})"),
+        firstRegex = new RegExp("{@" + regex + ".*?}"),
+        partialRegex = new RegExp("{@" + regex + " ");
+    if(description.match(fullRegex)){
+        description = description.split(lookaheadRegex);
         description.forEach(filter=>{
-            if(filter.match(/{@filter.*?}/)){
-                let filteredArray = jsplit(filter.split(/{@filter /)[1],/\|.*?}/g, 1);
+            if(filter.match(firstRegex)){
+                let filteredArray
+                if(!isSimple) filteredArray = jsplit(filter.split(partialRegex)[1], /\|.*?}/g, 1);
+                else filteredArray = jsplit(filter.split(partialRegex)[1], /}/g, 1);
                 filteredArray.forEach(piece =>{
                     filteredMessage += piece;
                 })
             } else filteredMessage += filter;
-            
         })
     } else return input;
     return filteredMessage;
