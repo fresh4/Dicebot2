52c3ba63fa89a99b1d6aaba25df56fc3af7b0d2b
Fresh <fresh1736@gmail.com>
2019-10-02 16:05:31 -0500

    Fixed bug with name rolling on a 100

--- functions/tableFunctions.js
+++ functions/tableFunctions.js
@@ -2,6 +2,7 @@ var RNG = require('./diceFunctions.js');
 exports.rollTable = function(table, diceType){
     let roll = RNG.RollSingle(diceType)
     for(let i in table){
+        if(table[i].max != undefined && table[i].max == 0) table[i].max = 100
         if(table[i].min && table[i].max){
             if(table[i].min <= roll && roll <= table[i].max){
                 if(table[i].result) return table[i].result
