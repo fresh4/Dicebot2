3f729c9769f77e7157f107f2e4a1cd279a9d3e4e
Fresh <fresh1736@gmail.com>
2019-10-01 22:37:40 -0500

    Added confirmation reply to setprefix

--- commands/setprefix.js
+++ commands/setprefix.js
@@ -1,5 +1,4 @@
 const fs = require('fs');
-//let serverConfig = require('../server_info.json');
 exports.run = (bot, msg, args) => {
     if(!msg.member.hasPermission("ADMINISTRATOR")){return msg.reply("You don't have permission to access this command.")}
     let prefix = JSON.parse(fs.readFileSync("./server_info.json", "utf8"));
@@ -7,6 +6,7 @@ exports.run = (bot, msg, args) => {
     fs.writeFileSync("./server_info.json", JSON.stringify(prefix, null));
     serverConfig = require('../server_info.json');
     bot.prefix = prefix;
+    msg.channel.send(`Server's prefix is now ${bot.prefix}`);
 }
 module.exports.help = {
     name: "setprefix",
