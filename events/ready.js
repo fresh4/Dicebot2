2a9a0628c7863e1b55c9b0f4db4b717e9f2b1259
Fresh <fresh1736@gmail.com>
2019-10-21 18:09:39 -0500

    Updated ready for logging

--- events/ready.js
+++ events/ready.js
@@ -1,4 +1,5 @@
 module.exports = (bot) => {
-    console.log(`Logged in and ready to roll on ${bot.guilds.size} servers!`)
+    var d = new Date();
+    console.log(`Logged in and ready to roll on ${bot.guilds.size} servers!\n${d.toDateString()} ${d.toTimeString()}`)
     bot.user.setActivity(`with your fate | !help`)
 }
