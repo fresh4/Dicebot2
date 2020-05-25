44eb2d545e9738f17231f3f400bbdfefc923bc47
Fresh <fresh1736@gmail.com>
2020-05-24 21:56:08 -0500

    Added sum total of rolls at the end

--- commands/multiroll.js
+++ commands/multiroll.js
@@ -5,11 +5,14 @@ exports.run = (bot, msg, args) => {
             msg.channel.send(`Invalid syntax for multiroll (remember the space). Syntax is ${bot.prefix}rr [number of iterations] [dice roll to iterate], ie (${bot.prefix}rr 6 d20+6)`);
             return;
         }
-        var loopCount = parseInt(args[0]), totalIterations = "\n";
+        var loopCount = parseInt(args[0]), totalIterations = "\n", sumOfAllRolls = 0;
         for(var i = 0; i < loopCount; i++){
-            totalIterations += `${args[1]}: ${dice.rollDiceFunc(args[1])}\n`
+            let roll = dice.rollDiceFunc(args[1]);
+            totalIterations += `${args[1]}: ${roll}\n`;
+            sumOfAllRolls += parseInt(roll.split('**')[1]);
         }
-        msg.channel.send(`Rolling ${loopCount} iterations: ${totalIterations}`);
+        msg.channel.send(`Rolling ${loopCount} iterations: ${totalIterations}`+
+                         `Total sum of rolls: \`${sumOfAllRolls}\``);
     }
     catch(Error){
         msg.channel.send(`Invalid roll. Try \`${bot.prefix}help roll\` for help.`);
