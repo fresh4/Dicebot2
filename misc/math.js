a87fc754c65051c80be7beaab4f8e427acfbd12d
Fresh <fresh1736@gmail.com>
2020-05-24 23:50:46 -0500

    Patched Undesired feature

--- misc/math.js
+++ misc/math.js
@@ -4,6 +4,7 @@ exports.badMath = function(bot, msg){
     if(!message.match(/[+\-*\/\^sqrt]/g) || message.startsWith(bot.prefix)) return
     try{
         result = Math.evaluate(message)
+        if(isNaN(result)) throw error
         msg.channel.send(result)
     } catch(error){
         return
