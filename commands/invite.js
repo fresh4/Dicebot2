0a7a985054f836e1404d7e80ff6f51e633fb7a9d
Fresh <fresh1736@gmail.com>
2020-05-24 21:57:45 -0500

    Added invite command

---
+++ commands/invite.js
@@ -0,0 +1,22 @@
+var discord = require('discord.js')
+exports.run = (bot, msg, args) => {
+
+    let embed = new discord.RichEmbed();
+    let invite = "https://discordapp.com/oauth2/authorize?client_id=392081019798421515&scope=bot&permissions=0";
+
+    embed.setTitle(`Click here to invite me to your server`)
+         .setURL(`${invite}`)
+         .setThumbnail(bot.user.avatarURL)
+         .setAuthor(msg.author.username, msg.author.avatarURL)
+         .setFooter(`Join the other ${bot.guilds.size} servers and invite me!`, bot.user.avatarURL)
+         .setColor("33D7FF")
+
+    msg.channel.send(embed)
+}
+module.exports.help = {
+    name: "invite",
+    category: "Administrative",
+    description: "Invite me to another server!",
+    detailedDesc: "",
+    usage: "invite"
+}
