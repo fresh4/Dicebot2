a665011cfb1418778a2577f4755f304bb977709b
Fresh <fresh1736@gmail.com>
2020-09-25 05:05:59 -0500

    Added optional features to classfeat lookup

--- commands/classfeat.js
+++ commands/classfeat.js
@@ -1,13 +1,18 @@
 var classes = require('../functions/loadFunctions.js').loadClassFeatures();
 var subclasses = require('../functions/loadFunctions.js').loadSubclassFeatures();
+var optionalFeats = require('../5eTools/data/optionalfeatures.json');
 var lookup = require('../functions/lookupFunctions.js')
 exports.run = (bot, msg, args) => {
     let features = {"feature": []};
     classes.classfeatures.forEach(feature =>{
-        features.feature.push(feature)
+        //if(!feature.source.match(/(Twitter|Stream|UA)/))
+            features.feature.push(feature)
     })
     subclasses.subclassfeatures.forEach(feature =>{
         if(feature) features.feature.push(feature)
+    })
+    optionalFeats.optionalfeature.forEach(feature =>{
+        features.feature.push(feature)
     })
 
     lookup.lookup(features, msg, args)
