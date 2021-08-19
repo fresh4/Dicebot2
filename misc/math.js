6be3a604b83df21be958553d784cd0218ce76dad
fresh4 <fresh1736@gmail.com>
2021-08-19 14:07:08 -0500

    Fixed Math again idk

--- misc/math.js
+++ misc/math.js
@@ -1,7 +1,7 @@
 var Math = require("mathjs");
 exports.badMath = function(bot, msg){
     var message = msg.content.toLowerCase(), result
-    if(/*!message.match(/[+\-*\/\^sqrt]/g) || */message.startsWith(bot.prefix)) return
+    if(!message.match(/[+\-*\/\^sqrt]/g) || message.startsWith(bot.prefix)) return
     try{
         result = Math.evaluate(message)
         if(isNaN(result)) throw error
