1f8ae37fccc7b5a46a60eba05247604fd122fb63
fresh4 <fresh1736@gmail.com>
2021-11-08 21:15:52 -0600

    Reimplimented full name generation (WIP)

--- commands/name.js
+++ commands/name.js
@@ -2,27 +2,44 @@ var roll = require('../functions/tableFunctions.js');
 var RNG = require('../functions/RNGFunctions.js');
 var names = require('../5eTools/data/names.json');
 exports.run = (bot, msg, args) => {
+    //args 0: race. args 1: gender/list. args 2: list
     let output = "Invalid arguments.";
     if(args.length < 1) return msg.channel.send("Specify a race.")
     names.name.forEach(race => {
         if(race.name.toLowerCase() == args[0].toLowerCase()){
-            output = ""
+            race = race.tables
             if(args.length == 1){
-                let index = RNG.rand(0, race.tables.length)
-                output = roll.rollTable(race.tables[index].table, race.tables[index].diceType)
-            } else if(args.length == 2){
+                output = generateList(race, 1)
+            } 
+            else if(args.length == 2){
                 if(args[1].toLowerCase() == "list"){
-                    for(let j = 0; j < 10; j++){
-                        let index = RNG.rand(0, race.tables.length)
-                        output += roll.rollTable(race.tables[index].table, race.tables[index].diceType) +"\n"
-                    }
+                    output = generateList(race, 10)
                 }
-            } else if(args.length == 3){
+                else {
+                    var table = selectGenderTable(race, args[1].toLowerCase())
+                    output = generateList(table, 1)    
+                }
+            } 
+            else if(args.length == 3 && args[2].toLowerCase() == "list"){
+                var table = selectGenderTable(race, args[1].toLowerCase())
+                output = generateList(table, 10)
             }
-            msg.channel.send(output)
         }
     })
-        
+    msg.channel.send(output)
+}
+function selectGenderTable(race, gender) {
+    if(gender == "female") gender = "Female"
+    else if (gender == "male") gender == "Male"
+    return race.filter(type => type.option.includes(gender))
+}
+function generateList(race, length){
+    let output = ""
+    for(let j = 0; j < length; j++){
+        let index = RNG.rand(0, race.length)
+        output += roll.rollTable(race[index].table, race[index].diceType) +"\n"
+    }
+    return output
 }
 module.exports.help = {
     name: "name",
